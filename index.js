5811a4312e135f4ba20d12bf5f16d1aa7b77c825
Tomohiro Matsuo <tomohmat@yahoo-corp.jp>
2020-07-23 04:22:09 +0900

    Add base

--- index.js
+++ index.js
@@ -4,44 +4,10 @@ const csv = require('csv');
 const parse = require('csv-parse/lib/sync');
 const base26 = require('base26');
 const Iconv = require('iconv').Iconv;
-
-// const parser = csv.parse((error, data) => {
-
-//   const targetColumns = [
-//     {
-//       column: "f",
-//       action: str => str.match(/1200\d{11}/) ? str.match(/1200\d{11}/)[0] : ""
-//     },
-//     {
-//       column: "p",
-//       action: str => `PB ${str}`
-//     }
-//   ]
-
-//   const result = data.map((row, i) => {
-//     // if (i === 0) return targetColumns.map(target => row[base26.from(target.column) - 1])
-//     if (i === 0) return []
-//     return targetColumns.map(target => target.action(row[base26.from(target.column) - 1]))
-//   })
-//   console.log(result);
-
-//   const writableStream = fs.createWriteStream('output.csv', { encoding: 'utf-8' });
-//   csv.stringify(result, (err, output) => {
-//     writableStream.write(output);
-//   });
-
-// })
+const readdirAsync = util.promisify(fs.readdir);
 
 (async () => {
-  const filepath = 'test.csv'
-  const csvObj = fs.readFileSync(filepath);
-  if (!csvObj) { throw new Error('cannot find file'); }
-
-  const iconv = new Iconv('sjis', 'utf-8//TRANSLIT//IGNORE');
-  const converted = iconv.convert(csvObj);
-  const data = parse(converted);
-
-  const targetColumns = [
+  const targetColumnsPB = [
     {
       column: "f",
       action: str => str.match(/1200\d{11}/) ? str.match(/1200\d{11}/)[0] : ""
@@ -51,17 +17,44 @@ const Iconv = require('iconv').Iconv;
       action: str => `PB ${str}`
     }
   ]
+  const targetColumnsPP = [
+    {
+      column: "f",
+      action: str => str.match(/1200\d{11}/) ? str.match(/1200\d{11}/)[0] : ""
+    },
+    {
+      column: "p",
+      action: str => `PP ${str}`
+    }
+  ]
 
-  const result = data.map((row, i) => {
-    // if (i === 0) return targetColumns.map(target => row[base26.from(target.column) - 1])
-    if (i === 0) return []
-    return targetColumns.map(target => target.action(row[base26.from(target.column) - 1]))
-  })
-  console.log(result);
+
+  const pbFiles = (await readdirAsync("PB")).filter(file => file.match(/csv$/)).map(file => `PB/${file}`);
+  const ppFiles = (await readdirAsync("PP")).filter(file => file.match(/csv$/)).map(file => `PP/${file}`);
+  const tasks = [...pbFiles.map(file => loadCSV(file, targetColumnsPB)), ...ppFiles.map(file => loadCSV(file, targetColumnsPP))]
+  const result = (await Promise.all(tasks)).flat()
 
   const output = await util.promisify(csv.stringify)(result);
 
   const outputPath = "result.csv"
   await util.promisify(fs.writeFile)(outputPath, output);
+  console.log("Finish export!");
 
 })()
+
+const loadCSV = async (filepath, targetColumns) => {
+  const csvObj = fs.readFileSync(filepath);
+  if (!csvObj) { throw new Error('cannot find file'); }
+
+  const iconv = new Iconv('sjis', 'utf-8//TRANSLIT//IGNORE');
+  const converted = iconv.convert(csvObj);
+  const data = parse(converted);
+
+  const result = data.map((row, i) => {
+    // if (i === 0) return targetColumns.map(target => row[base26.from(target.column) - 1])
+    if (i === 0) return []
+    return targetColumns.map(target => target.action(row[base26.from(target.column) - 1]))
+  }).filter(row => row.length > 0)
+  // console.log(result);
+  return result
+}
